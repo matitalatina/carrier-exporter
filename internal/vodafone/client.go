ddebc9b821fa414853f5b376bd1e54c5293ecce7
Mattia Natali <matitalatina@gmail.com>
2021-05-15 18:16:09 +0200

    Updated vodafone headers

--- internal/vodafone/client.go
+++ internal/vodafone/client.go
@@ -160,13 +160,13 @@ func getDefaultHeaders() http.Header {
 		"X-Bwb-AppId": {"MyVodafone"},
 		// These ids are random (32 chars)
 		// https://passwordsgenerator.net/
-		"X-Bwb-InstallationId": {"r02evmxqhcmffg9yd3qcojerk38tuqer"},
-		"X-Device-Id":          {"r02evmxqhcmffg9yd3qcojerk38tuqer"},
+		"X-Bwb-InstallationId": {"i0uifrsnao7zes0k1xkom0wg43iqsa5h"},
+		"X-Device-Id":          {"i0uifrsnao7zes0k1xkom0wg43iqsa5h"},
 		"clientId":             {"4002"},
-		"X-Bwb-User-Agent":     {"MyVodafone/android/s/10/12.4.0/2.625/store/\"samsung\"/\"SM-G970F\""},
-		"X-Device-UserAgent":   {"MyVodafone/android/s/10/12.4.0/2.625/store/\"samsung\"/\"SM-G970F\""},
-		"User-Agent":           {"Dalvik/2.1.0 (Linux; U; Android 10; SM-G970F Build/QP1A.190711.020)"},
-		"X-Bwb-AppVersion":     {"12.4.0"},
+		"X-Bwb-User-Agent":     {"MyVodafone/android/s/11/12.12.2/2.625/store/\"samsung\"/\"SM-G970F\""},
+		"X-Device-UserAgent":   {"MyVodafone/android/s/1/12.12.2/2.625/store/\"samsung\"/\"SM-G970F\""},
+		"User-Agent":           {"Dalvik/2.1.0 (Linux; U; Android 11; SM-G970F Build/QP1A.190711.020)"},
+		"X-Bwb-AppVersion":     {"12.12.2"},
 		"Content-Type":         {"application/json; charset=UTF-8"},
 		"X-Network-Info":       {"w/nd/0.71/222"},
 	}
